3a701a1f8f162be34a850809c927360797d0b0e1
Roman Movsisyan <roman.movsisian@gmail.com>
2019-08-08 07:47:30 +0300

    Added manage user endpoint.

--- app/core/models.py
+++ app/core/models.py
@@ -5,8 +5,9 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,\
 
 
 class UserManager(BaseUserManager):
+    """Custom user manager."""
     def create_user(self, email, password=None, **extra_fields):
-        """Creates and saves a new user"""
+        """Creates and saves a new user."""
         if not email:
             raise ValueError('Users must have an email adress')
         user = self.model(email=self.normalize_email(email), **extra_fields)
@@ -16,7 +17,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        """Creates and saves a new super user"""
+        """Creates and saves a new super user."""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -26,7 +27,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that supports using email instead of username"""
+    """Custom user model that supports using email instead of username."""
     email = models.EmailField(max_length=254, unique=True)
     name = models.CharField(max_length=50)
     is_active = models.BooleanField(default=True)
