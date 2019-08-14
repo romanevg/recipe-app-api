bc3174a6677d622472952b624c784f04dc2f4e81
Roman Movsisyan <roman.movsisian@gmail.com>
2019-08-14 14:07:13 +0300

    Added working list tags endpoint.

--- app/core/models.py
+++ app/core/models.py
@@ -2,10 +2,12 @@ from django.db import models
 # Используем обр. слэш для переноса строки для соответсвия длине 79 символов
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,\
     PermissionsMixin
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
     """Custom user manager."""
+
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user."""
         if not email:
@@ -36,3 +38,18 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Tag(models.Model):
+    """Tag to be used for a recipe."""
+    name = models.CharField(max_length=255)
+    # This is the recommended way to retrieve different settings
+    # from the Django settings.
+    # So this is the best practice for retrieving user from settings.
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
